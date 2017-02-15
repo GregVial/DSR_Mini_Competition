e377b88c6b3cd3ec3f1ec429dd2b95cbb7a1ec09
Adam Green <adam.green@adgefficiency.com>
2017-02-15 16:53:42 +0100

    multi core models

--- svm.py
+++ svm.py
@@ -40,73 +40,74 @@ from sklearn.model_selection import GridSearchCV
 # 'linear', 'poly', 'rbf', 'sigmoid'
 
 # NOT DOING CROSS VALIDATION
-print('Starting SVM Grid Search')
-svm_gs = GridSearchCV(s_vec, param_grid={'C':[1],'kernel':['linear', 'rbf']})
-svm_gs.fit(X_train, y_train)
+if __name__ == "__main__":
+    print('Starting SVM Grid Search')
+    svm_gs = GridSearchCV(s_vec, param_grid={'C':[1],'kernel':['linear']}, n_jobs=-1)
+    svm_gs.fit(X_train, y_train)
 
-s_vec = svm_gs.best_estimator_
-y_pred_svm = s_vec.predict(X_test)
-svm_accuracy = np.mean(y_pred_svm == y_test)
-svm_roc = roc_auc_score(y_test, y_pred_svm)
+    s_vec = svm_gs.best_estimator_
+    y_pred_svm = s_vec.predict(X_test)
+    svm_accuracy = np.mean(y_pred_svm == y_test)
+    svm_roc = roc_auc_score(y_test, y_pred_svm)
 
-print('SVM Accuracy is ' + str(svm_accuracy))
-print('SVM ROC Area Under Curve is ' + str(svm_roc))
+    print('SVM Accuracy is ' + str(svm_accuracy))
+    print('SVM ROC Area Under Curve is ' + str(svm_roc))
 
-from sklearn.metrics import confusion_matrix
-svm_conmat = np.array(confusion_matrix(y_test, y_pred))
-svm_confusion = pd.DataFrame(svm_conmat, index=['not_granted', 'granted'],columns=['predicted_not_granted', 'predicted_granted'])
+    from sklearn.metrics import confusion_matrix
+    svm_conmat = np.array(confusion_matrix(y_test, y_pred))
+    svm_confusion = pd.DataFrame(svm_conmat, index=['not_granted', 'granted'],columns=['predicted_not_granted', 'predicted_granted'])
 
-# LOGISTIC REGRESSION
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
+    # LOGISTIC REGRESSION
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.preprocessing import StandardScaler
 
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train_std = scaler.transform(X_train)
-X_test_std = scaler.transform(X_test)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_std = scaler.transform(X_train)
+    X_test_std = scaler.transform(X_test)
 
-log_r = LogisticRegression(penalty='l2', C=1.0, solver = 'sag', n_jobs=-1)
-log_r.fit(X_train_std, y_train)
-print('Starting net Grid Search')
-lr_gs = GridSearchCV(log_r, param_grid={'C':[1]})
-lr_gs.fit(X_train_std, y_train)
-log_r = lr_gs.best_estimator_
+    log_r = LogisticRegression(penalty='l2', C=1.0, solver = 'sag', n_jobs=-1)
+    log_r.fit(X_train_std, y_train)
+    print('Starting net Grid Search')
+    lr_gs = GridSearchCV(log_r, param_grid={'C':[1]})
+    lr_gs.fit(X_train_std, y_train)
+    log_r = lr_gs.best_estimator_
 
-y_pred_lr = log_r.predict(X_test_std) #
-lr_accuracy = np.mean(y_pred_lr == y_test)
-lr_roc = roc_auc_score(y_test, y_pred_lr)
+    y_pred_lr = log_r.predict(X_test_std) #
+    lr_accuracy = np.mean(y_pred_lr == y_test)
+    lr_roc = roc_auc_score(y_test, y_pred_lr)
 
-print('LR Accuracy is ' + str(lr_accuracy))
-print('LR ROC Area Under Curve is ' + str(lr_roc))
+    print('LR Accuracy is ' + str(lr_accuracy))
+    print('LR ROC Area Under Curve is ' + str(lr_roc))
 
-# neural network
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import adam
+    # neural network
+    from keras.models import Sequential
+    from keras.layers import Dense, Dropout
+    from keras.optimizers import adam
 
-input_length = X_train.shape[1]
+    input_length = X_train.shape[1]
 
-# defining layers for prediction network
-pred_net_params = pred_net_D['model']  # change to pred_net_D[mdl] if using different mdl structure for each run
-input_layer = Dense(500, input_dim=input_length, activation='relu')
-hidden_layer = Dense(500, activation='relu')
-output_layer = Dense(output_dim=1, activation='linear')
+    # defining layers for prediction network
+    pred_net_params = pred_net_D['model']  # change to pred_net_D[mdl] if using different mdl structure for each run
+    input_layer = Dense(500, input_dim=input_length, activation='relu')
+    hidden_layer = Dense(500, activation='relu')
+    output_layer = Dense(output_dim=1, activation='linear')
 
-# creating keras model
-network = Sequential()
-network.add(input_layer)
-network.add(Dropout(dropout_fraction))
-network.add(hidden_layer)
-network.add(Dropout(dropout_fraction))
-network.add(hidden_layer)
-network.add(Dropout(dropout_fraction))
-network.add(output_layer)
-network.compile(loss='mean_squared_error', optimizer=adam)
+    # creating keras model
+    network = Sequential()
+    network.add(input_layer)
+    network.add(Dropout(dropout_fraction))
+    network.add(hidden_layer)
+    network.add(Dropout(dropout_fraction))
+    network.add(hidden_layer)
+    network.add(Dropout(dropout_fraction))
+    network.add(output_layer)
+    network.compile(loss='mean_squared_error', optimizer=adam)
 
-history = network.fit(X_train_std, y_train)
-y_pred_net = network.predict(X_test_std)
-net_accuracy = np.mean(y_pred_net == y_test)
-net_roc = roc_auc_score(y_test, y_pred_net)
+    history = network.fit(X_train_std, y_train)
+    y_pred_net = network.predict(X_test_std)
+    net_accuracy = np.mean(y_pred_net == y_test)
+    net_roc = roc_auc_score(y_test, y_pred_net)
 
-print('NN Accuracy is ' + str(net_accuracy))
-print('NN ROC Area Under Curve is ' + str(net_roc))
+    print('NN Accuracy is ' + str(net_accuracy))
+    print('NN ROC Area Under Curve is ' + str(net_roc))
