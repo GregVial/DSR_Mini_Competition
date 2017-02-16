b5a2640742bafb5a71125fb13ece15b9757b6e6a
Gregory Vial <greg_vial@hotmail.com>
2017-02-16 11:13:09 +0100

    imputing

--- imputation.py
+++ imputation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
 
 raw = pd.read_csv('data/unimelb_training.csv')
 #
@@ -10,28 +11,31 @@ raw = pd.read_csv('data/unimelb_training.csv')
 
 df = raw.ix[:,2:5]
 
-df.dropna(axis=0, inplace=True)
+df.dropna(axis=0, subset=["Sponsor.Code","Grant.Category.Code"],inplace=True)
+
 na_dummy_bool=True
 df = pd.concat([df,pd.get_dummies(df["Sponsor.Code"],prefix="Sponsor", dummy_na=na_dummy_bool)],axis=1)
-df.drop("Sponsor.Code",axis=1,inplace=True)
+df = pd.concat([df,pd.get_dummies(df['Grant.Category.Code'],prefix="Grant", dummy_na=na_dummy_bool)],axis=1)
 
-df = pd.concat([df,pd.get_dummies(df['Grant.Category.Code'],prefix="Sponsor", dummy_na=na_dummy_bool)],axis=1)
+df.drop("Sponsor.Code",axis=1,inplace=True)
 df.drop('Grant.Category.Code',axis=1,inplace=True)
+#df.drop('Contract.Value.Band...see.note.A',axis=1,inplace=True)
+
 
 # df = pd.concat([data,pd.get_dummies(data['Contract.Value.Band...see.note.A'],prefix="Sponsor", dummy_na=na_dummy_bool)],axis=1)
 # df.drop("Contract.Value.Band...see.note.A",axis=1,inplace=True)
 
-X_train = df.ix[:,1:].astype(str)
-y_train = df.ix[:,0].astype(str)
+X_train,X_test,y_train,y_test = train_test_split(df.ix[:,1:].astype(str),df.ix[:,0].astype(str))
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 gbc = GradientBoostingClassifier(random_state=0)
-params={
-    "n_estimators":[500],
-    "min_samples_split":[5],
-    "min_samples_leaf":[4]
-}
-if __name__ == "__main__":
-    gbc.fit(X_train,y_train)
+
+#if __name__ =="__main__":
+#    gbc.fit(X_train,y_train)
+
+gbc.fit(X_train,y_train)
+
+y_pred = gbc.predict(X_test)
+print(np.mean(y_test==y_pred))
